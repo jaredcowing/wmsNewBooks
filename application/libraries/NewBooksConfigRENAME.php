f68b886da549408ad8df65226bf2865c736397a7
Jared Cowing <jaredcowing@gmail.com>
2019-06-18 16:48:09 -0700

    Update NewBooksConfigRENAME.php
    New fields added to better customize receipt date settings.

--- application/libraries/NewBooksConfigRENAME.php
+++ application/libraries/NewBooksConfigRENAME.php
@@ -20,4 +20,17 @@ class NewBooksConfig {
 			);
 			return $branchesArr;
 		}
+		
+		public function getStatute(){
+			return "2018-07-01";																			//Books ordered before this date won't continually be checked in on ("we give up, that book isn't ever coming" or "that record may be a data entry error")
+		}
+		
+		public function getMonthPad(){
+			return 3;																						//If you will determine "availability date" of books by their order date, enter here the number of months it typically takes from order to shelf-ready. SET TO ZERO IF YOU WILL BE USING RECEIPT DATE INSTEAD. This helps the application to estimate an "arrival date" (ready date) from patron's perspective.
+		}
+		
+		public function getAgeDeterminant(){																
+			return 'order';																					//Set string to 'order' if you would like availability dates to be estimated based on their order date.
+																											//Set string to 'receipt' if you would like application to use the date it first noticed an item record was created. Don't start using 'receipt' until after your application has been running for a few months; the first time you load orders, all items received by that time will get 'dateAppear' set to that day (old items will appear to be new).
+		}
 }
